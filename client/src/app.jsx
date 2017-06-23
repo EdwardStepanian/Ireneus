217dfa07a2a9fd9b293465fe00a99f4345fbacc4
EdwardStepanian <edward.stepanian.se@gmail.com>
2017-06-23 15:29:19 +0400

    Create frontpage component using Container and Presentation components pattern

---
+++ client/src/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import HomePage from './components/HomePage';
+
+const root = document.getElementById('root');
+
+ReactDom.render(<HomePage/>, root);
---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Card, CardTitle} from 'material-ui/Card';
+
+const HomePage = ()=> (
+
+	<Card className = "container">
+		<CardTitle title = "ReactApp" />
+	</Card>
+
+)
+
+export default HomePage;
