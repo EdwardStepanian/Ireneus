2e0ca92666c3eecb0e84216a8c6d73774d414f67
EdwardStepanian <edward.stepanian.se@gmail.com>
2017-06-26 11:58:34 +0400

    processing data form

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,5 +5,53 @@ const router = new express.Router();
 //Validate SignUp Form
 
 function validateSignupForm(payload) {
+    let errors = {},
+        isFormValid = true,
+        message = '';
 
+    if(!payload || typeof payload.email !== 'string' || !validator.isEmail(payload.email)){
+        isFormValid = false;
+        errors.email = 'Please provide your email address';
+    }
+    if (!payload || typeof payload.password !== 'string' || payload.password.trim().length === 0) {
+        isFormValid = false;
+        errors.password = 'Please provide your password.';
+    }
+
+    if (!isFormValid) {
+        message = 'Check the form for errors.';
+    }
+
+    return {
+        success: isFormValid,
+        message,
+        errors
+    };
 }
+
+router.post('/signup', (req, res) => {
+    const validationResult = validateSignupForm(req.body);
+    if(!validationResult.success){
+        return res.status(400).json({
+            success: false,
+            message: validationResult.message,
+            errors: validationResult.errors
+        })
+    }
+    return res.status(200).end();
+})
+router.post('/login', (req, res) => {
+    const validationResult = validateLoginForm(req.body);
+    if (!validationResult.success) {
+        return res.status(400).json({
+            success: false,
+            message: validationResult.message,
+            errors: validationResult.errors
+        });
+    }
+
+    return res.status(200).end();
+});
+
+
+module.exports = router;
