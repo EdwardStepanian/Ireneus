5b12eb58b44de6783f31ba24cc6aa14713fa483f
EdwardStepanian <edward.stepanian.se@gmail.com>
2017-06-26 20:32:37 +0400

    passport local strategy,eslintrc file

---
+++ server/passport/local-signup.js
@@ -0,0 +1,24 @@
+const User = require("mongoose")
+const passportLocal = require("passport-local");
+const userModel = User.model("User")
+const passportStrategy = passportLocal.Strategy
+
+module.exports = new passportStrategy({
+    usernameField: "email",
+    passwordField: "password",
+    session: false,
+    passReqToCallback: true
+}, (req, email, password, done) => {
+    const userData = {
+        email: email.trim(),
+        password: password.trim(),
+        name: req.body.name.trim()
+    };
+
+    const newUser = new User(userData);
+    newUser.save((err) => {
+        if (err) { return done(err); }
+
+        return done(null);
+    });
+});
