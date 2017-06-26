c34a19d448b07ec968e86b9d98cf83c788d9153a
EdwardStepanian <edward.stepanian.se@gmail.com>
2017-06-26 13:48:37 +0400

    Ajax requests to our SignUp system

--- client/src/containers/SignUpPage.jsx
+++ client/src/containers/SignUpPage.jsx
@@ -29,10 +29,43 @@ class SignUpPage extends React.Component{
             user
         });
     }
-    //Process the form
 
+    //Process the form
     processForm(event){
         event.preventDefault();
+
+        // create a string for an HTTP body message
+        const name = encodeURIComponent(this.state.user.name);
+        const email = encodeURIComponent(this.state.user.email);
+        const password = encodeURIComponent(this.state.user.password);
+        const formData = `name=${name}&email=${email}&password=${password}`;
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('post', '/auth/signup');
+        xhr.userRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.responseType = 'json';
+        xhr.addEndEventListener('load', () => {
+                if( xhr.status === 200 ) {
+                    //success
+
+                    //Change component-container state
+                    this.setState({
+                        errors : {}
+                    });
+                    console.log('The form is valid');
+                }else {
+                    // failure
+                    const errors = xhr.response.errors ? xhr.response.errors : {};
+                    errors.summary = xhr.response.message;
+
+                    this.setState({
+                        errors
+                    });
+                }
+            });
+
+        xhr.send(formData);
+
 
         console.log('name:', this.state.user.name);
         console.log('email:', this.state.user.email);
