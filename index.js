c501081057fe8fc32514ab7b64806b68ec72e46d
EdwardStepanian <edward.stepanian.se@gmail.com>
2017-06-26 18:08:21 +0400

    initialize passport library and add new express routes

--- index.js
+++ index.js
@@ -41,9 +41,13 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const port = 3002;
 const domainName = 'Ireneus';
 const app = express();
+
+// connect to the database
+require('./server/models').connect(config.dbUri);
 
 app.use(express.static('./server/static'));
 app.use(express.static('./client/dist'));
@@ -52,7 +56,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // routes
 const authRoutes = require('./server/routes/auth');
+const apiRoutes = require('./server/routes/api');
 app.use('/auth', authRoutes);
+app.use('/api', apiRoutes);
+
+app.use(passport.initialize());
+// load passport strategies
+const localSignupStrategy = require('./server/passport/local-signup');
+const localLoginStrategy = require('./server/passport/local-login');
+passport.use('local-signup', localSignupStrategy);
+passport.use('local-login', localLoginStrategy);
+
+// pass the authenticaion checker middleware
+const authCheckMiddleware = require('./server/middleware/auth-check');
+app.use('/api', authCheckMiddleware);
+
 
 // Start the server
 app.listen(port,domainName, () => {
