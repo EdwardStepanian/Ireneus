41f61012532b4bf6a1bb4ec3de3b3d899f44706f
Edward <edward.stepanian@astgeek.com>
2017-07-09 17:26:21 +0400

    Conditon for deciding which menu should be shown

--- client/src/components/Base.jsx
+++ client/src/components/Base.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link, IndexLink} from 'react-router';
+import Auth from '../modules/Auth';
+
 
 const Base = ({
     children
@@ -9,11 +11,19 @@ const Base = ({
             <div className="top-bar-left">
                 <IndexLink to="/">Ireneus </IndexLink>
             </div>
+            {Auth.isUnauthenticated() ? (
+                <div className="top-bar-right">
+                    <Link to="/logout" > Log out </Link>
+                </div>
+                ): (
             <div className="top-bar-right">
                 <Link to="/login">Log In </Link>
                 <Link to="/signup">Sign Up </Link>
             </div>
+                )}
         </div>
+        {/*Render Here a children component*/}
+        {console.log(children)}
         {children}
     </div>
 );
